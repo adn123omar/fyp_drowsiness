0e187ab0bc01e320d3f7be59fae56fe99f631461
Umar Abedin <adn123omar@gmail.com>
2023-10-02 16:23:35 +1100

    csscd

--- ID_SORTED/mrmr_speedrun.py
+++ ID_SORTED/mrmr_speedrun.py
@@ -4,16 +4,16 @@ from sklearn.feature_selection import SelectKBest, f_regression
 
 if __name__ =="__main__":
     # Loading Dataset as pandas
-    dataset = pd.read_csv("features_and_labels_30s.csv")
+    dataset = pd.read_csv("features_and_labels_15s.csv")
     X = dataset.iloc[:,0:-2]
     y = dataset.iloc[:,-1]
 
     # MRMR selection
-    selected_features_MRMR = mrmr_regression(X=X,y=y, K=15)
+    selected_features_MRMR = mrmr_regression(X=X,y=y, K=50)
     print(selected_features_MRMR)
 
     # SelecKBest selection
-    selector = SelectKBest(f_regression, k=15)
+    selector = SelectKBest(f_regression, k=50)
     selector.fit(X, y)
     cols_idxs = selector.get_support(indices=True)
     features_df_new = X.iloc[:,cols_idxs]
